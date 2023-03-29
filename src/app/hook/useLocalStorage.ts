84c161659c883eaa7ceabeca0d9ca3b0f646ff3b
musyrifp8 <bilalabdurrahman.work@gmail.com>
2023-03-29 15:20:12 +0700

    feat: integrate search func

---
+++ src/app/hook/useLocalStorage.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export enum ILocalStorageItems {
+  CONTACTS = "CONTACTS",
+  TOTAL_DATA = "TOTAL_DATA",
+  CURRENT_PAGE = "CURRENT_PAGE",
+  FILTER = "FILTER",
+}
+
+function useLocalStorage<T>(
+  key: ILocalStorageItems,
+  initialValue: T
+): [T, (value: T) => void] {
+  const [value, setValue] = useState(() => {
+    const storedValue =
+      typeof window !== "undefined" ? localStorage.getItem(key) : null;
+    return storedValue !== null ? JSON.parse(storedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
+
+export default useLocalStorage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,152 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { Inter } from 'next/font/google'
+import { useEffect, useState } from "react";
+import { Inter } from "next/font/google";
 import client from "./graphql/apollo-client";
-import { GET_CONTACTS, GET_TOTAL_CONTACTS } from './graphql/query';
-import { Table, Pagination } from 'antd';
-import { ColumnsType } from 'antd/es/table';
+import { GET_CONTACTS, GET_TOTAL_CONTACTS } from "./graphql/query";
+import { Table, Pagination, Spin, Input } from "antd";
+import { ColumnsType } from "antd/es/table";
+import useLocalStorage, { ILocalStorageItems } from "./hook/useLocalStorage";
+
+const { CONTACTS, CURRENT_PAGE, TOTAL_DATA, FILTER } = ILocalStorageItems;
 
 // const inter = Inter({ subsets: ['latin'] })
 
 interface Contact {
-  created_at: string,
-  first_name: string,
-  id: 2452,
-  last_name: string,
+  created_at: string;
+  first_name: string;
+  id: 2452;
+  last_name: string;
   phones: {
-    number: number
-  }[]
+    number: number;
+  }[];
 }
 
-
+interface IPagination {
+  currentPage: number;
+  offset: number;
+}
 
 export default function Home() {
 
-  // const data = await getData()
-
-  const [contacts, setContacts] = useState<Contact[]>([])
-  const [currentPage, setCurrentPage] = useState<number>(1)
-  const [totalData, setTotalData] = useState<number>(0)
+  const [contacts, setContacts] = useLocalStorage<Contact[]>(CONTACTS, []);
+  const [pagination, setPagination] = useLocalStorage<IPagination>(
+    CURRENT_PAGE,
+    { currentPage: 1, offset: 0 }
+  );
+  const [totalData, setTotalData] = useLocalStorage<number>(TOTAL_DATA, 0);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [filter, setFilter] = useLocalStorage<string>(FILTER, '')
 
   const columns: ColumnsType<any> = [
     {
-      dataIndex: 'first_name',
-      key: 'first_name',
+      dataIndex: "name",
+      key: "name",
       render: (_, record: Contact) => {
-        console.log(record)
-        return record.first_name
-      }
-    }, {
-      dataIndex: 'phones',
-      key: 'phones',
+        const name = `${record.first_name} ${record.last_name}`
+        return `${name.length - 15 <= 0 ? `${name}` : `${name.substring(0,15)}...`}`;
+      },
+    },
+    {
+      dataIndex: "phones",
+      key: "phones",
       render: (_, record: Contact) => {
-        console.log(record)
-        return record.phones[0].number
-      }
-    }
-  ]
+        return record.phones[0].number;
+      },
+    },
+  ];
 
-  async function getData(limit: number, offset: number): Promise<{
-    contacts: Contact[], totalData: number
+  async function getData(
+    limit: number,
+    offset: number
+  ): Promise<{
+    contacts: Contact[];
+    totalData: number;
   }> {
     const contacts = await client.query({
       variables: {
         limit,
         offset,
-        // where: {
-        //   first_name: { "_like": "%sd%" },
-        //   last_name: { "_like": "%sd%" }
-        // }
+        where: {
+          _or: [
+          
+            {first_name: { "_like": `%${filter}%` }},
+            {last_name: { "_like": `%${filter}%` }}
+        ]
+        }
       },
       query: GET_CONTACTS,
-      
     });
 
     const total = await client.query({
-      query: GET_TOTAL_CONTACTS
-    })
-  
-    return {contacts: contacts.data.contact, totalData: total.data.contact_aggregate.aggregate.count}
+      variables: {
+        where: {
+          _or: [
+          
+            {first_name: { "_like": `%${filter}%` }},
+            {last_name: { "_like": `%${filter}%` }}
+        ]
+        }
+      },
+      query: GET_TOTAL_CONTACTS,
+    });
+
+    return {
+      contacts: contacts.data.contact,
+      totalData: total.data.contact_aggregate.aggregate.count,
+    };
   }
-  
+
   async function onPageChange(page: number): Promise<void> {
-    console.log(page)
+    let tempOffset = pagination.offset
+    if (pagination.currentPage > page) {
+      tempOffset = tempOffset - 10
+    } else {
+      tempOffset = tempOffset + 10
+    }
+    setPagination({ currentPage: page, offset: tempOffset });
   }
+
 
   useEffect(() => {
     const init = async () => {
-      const data = await getData(10, 0)
-      setTotalData(data.totalData)
-      setContacts(data.contacts)
-    }
-    init()
-  }, [])
-  
-  // console.log(data)
+        try {
+          const data = await getData(10, pagination.offset);
+          setContacts(data.contacts);
+          setTotalData(data.totalData);
+        } catch (error) {
+          console.log(error);
+        }
+      
+      setTimeout(() => setIsLoading(false), 3000)
+    };
+
+    init();
+  }, [pagination, filter]);
+
   return (
-    <main>
-      {/* <ul>{contacts?.map(item => <li key={item.id}>{item.first_name}</li>)}</ul> */}
-      <Table columns={columns} showHeader={false} pagination={false} dataSource={contacts || []} />
-      <Pagination current={currentPage} total={totalData} onChange={(page) => onPageChange(page) } />
-    </main>
+    <div>
+      <Input.Search placeholder="Search by name" value={filter} onChange={e => setFilter(e.target.value)} loading={isLoading} />
+      {isLoading ? (
+        <div>
+          <Spin spinning={isLoading} />
+        </div>
+      ) : (
+        <div>
+          <Table
+            columns={columns}
+            showHeader={false}
+            pagination={false}
+            dataSource={contacts || []}
+          />
+
+          <Pagination
+            current={pagination.currentPage}
+            total={totalData}
+            onChange={(page) => onPageChange(page)}
+          />
+        </div>
+      )}
+    </div>
   )
 }
